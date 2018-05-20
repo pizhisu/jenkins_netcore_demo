2dd07ab191d4d628b11cd8735b3574ce5bf4e077
pizhisu <812512286.@qq.com>
2018-05-20 12:40:34 +0800

    test

---
+++ JenkinsTest/JenkinsTest/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace JenkinsTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseUrls("http://*:3030")
+                .UseStartup<Startup>()
+                .Build();
+    }
+}
